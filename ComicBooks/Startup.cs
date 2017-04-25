5f52da56c2ec3b5c4e3960dc3b2a9c3d6be80c02
Luke Norris <luken22@hotmail.co.uk>
2017-04-25 17:09:46 +0100

    First Project Commit

---
+++ ComicBooks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ComicBooks.Startup))]
+namespace ComicBooks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
